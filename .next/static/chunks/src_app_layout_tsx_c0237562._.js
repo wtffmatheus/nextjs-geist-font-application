4c77d37af1c70d79f99c994e649c584d35730910
wtffmatheus <97193172+wtffmatheus@users.noreply.github.com>
2025-06-08 16:15:34 -0300

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1749410121537

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__a6014fe5._.css",
+  "static/chunks/node_modules_13b5752c._.js",
+  "static/chunks/src_25786446._.js"
+],
+    source: "dynamic"
+});
